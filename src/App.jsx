e695036f655c130f95148e1e0997debb9a9864d5
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-12-23 12:38:42 +0530

    feat: router navigation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link, Routes, Route, useLocation } from "react-router-dom";
+import CountryList from "./pages/CountryList";
+import CountryDetails from "./pages/CountryDetails";
+import Favorites from "./pages/Favorites";
 
 const App = () => {
-  return <div className="bg-blue-400">Appdsjfkldsjflds</div>;
+  const location = useLocation();
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <nav className="bg-white shadow p-4 flex justify-between">
+        <Link to="/" className="font-bold text-xl">
+          Country Explorer
+        </Link>
+
+        {/* Show Favorites button only if not on /favorites page */}
+        {location.pathname !== "/favorites" && (
+          <Link
+            to="/favorites"
+            className="text-white bg-linear-to-r from-blue-500 to-blue-700 px-4 py-2 rounded font-medium"
+          >
+            Favorites
+          </Link>
+        )}
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<CountryList />} />
+        <Route path="/country/:name" element={<CountryDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
