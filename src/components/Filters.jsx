19e00c64374ad153cf0452d953028110ab7a500a
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-12-23 15:20:18 +0530

    feat: dark mode

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,7 +5,7 @@ const Filters = ({ region, setRegion, regions }) => {
     <select
       value={region}
       onChange={(e) => setRegion(e.target.value)}
-      className="p-2 mb-4 border rounded-lg focus:outline-none focus:ring-1"
+      className="p-2 mb-4 border rounded-lg focus:outline-none dark:bg-gray-700 dark:text-white focus:ring-1"
     >
       <option value="">All Regions</option>
       {regions.map((r) => (
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,7 +17,7 @@ const SearchBar = ({ searchTerm, setSearchTerm, delay = 300 }) => {
       placeholder="Search country..."
       value={inputValue}
       onChange={(e) => setInputValue(e.target.value)}
-      className="w-full max-w-md p-2 mb-4 border rounded-lg focus:outline-none focus:ring-1"
+      className="w-full max-w-md p-2 mb-4 border rounded-lg dark:border-white dark:placeholder:text-white dark:text-white focus:outline-none focus:ring-1"
     />
   );
 };
